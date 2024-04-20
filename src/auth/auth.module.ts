dc1a10a1de69c2be4c1cf1465efb2fe4fbf0d3a6
Chatnipat puriniput <chatnipat.p@gmail.com>
2024-04-20 13:49:36 +0700

    add: auth with jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,41 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
 import { PrismaService } from 'src/prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.stretegy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService, UserService, PrismaService],
+  providers: [
+    AuthService,
+    UserService,
+    PrismaService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: parseInt(
+            configService.getOrThrow<string>(
+              'ACCESS_TOKEN_VALIDITY_DURATION_IN_SEC',
+            ),
+          ),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,36 @@
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(user: Prisma.UserUncheckedCreateInput) {
+    return {
+      access_token: this.jwtService.sign({
+        email: user.email,
+        id: user.id,
+      }),
+    };
+  }
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const existingUser = await this.userService.findOneByEmail(email);
+    if (!existingUser) {
+      throw new BadRequestException('User not found');
+    }
+    const isMatch = await bcrypt.compareSync(password, existingUser.password);
+    if (!isMatch) {
+      throw new BadRequestException('Password incorrect');
+    }
+    return existingUser;
+  }
 
   async register(user: Prisma.UserCreateInput) {
     const existingUser = await this.userService.findOneByEmail(user.email);
@@ -13,6 +38,10 @@ export class AuthService {
       throw new BadRequestException('email already exist');
     }
     const hashedPassword = await bcrypt.hash(user.password, 10);
-    return this.userService.createUser({ ...user, password: hashedPassword });
+    const createdUser = await this.userService.createUser({
+      ...user,
+      password: hashedPassword,
+    });
+    return this.login(createdUser);
   }
 }
